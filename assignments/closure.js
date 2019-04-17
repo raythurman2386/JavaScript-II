b0a856ba9f19007c23577b6422c0baffc5dc69e1
Raymond Thurman <raymondthurman5@gmail.com>
2019-04-17 19:50:41 -0400

    challenge 1 closure complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function sayHello() {
+  const say = function () {
+    console.log(hello);
+  }
+  const hello = 'Hello, world!';
+  return say;
+}
+const sayHelloClosure = sayHello();
+sayHelloClosure();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 

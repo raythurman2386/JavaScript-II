436fa73aee6e28b3da5fed48f7b0f65b072da135
Raymond Thurman <raymondthurman5@gmail.com>
2019-04-17 20:40:20 -0400

    callbacks function invocations completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -30,20 +30,37 @@ function getLength(arr, cb) {
   return cb(arr.length);
 }
 
+getLength(items,
+  function (length) {
+    console.log(length);
+  });
+
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb((arr.length) - 1);
+  let lastElement = arr.pop();
+  return cb(lastElement);
 }
+
+last(items, function (last) {
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
 }
+sumNums(2, 3, function (nums) {
+  console.log(nums);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x * y);
 }
+multiplyNums(2, 3, function (nums) {
+  console.log(nums);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
@@ -53,8 +70,13 @@ function contains(item, list, cb) {
   }
 }
 
+contains('Gum', items, function (item) {
+  console.log(item);
+});
+
 
 /* STRETCH PROBLEM */
+const newItems = ['Pencil', 'Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
@@ -65,3 +87,7 @@ function removeDuplicates(array, cb) {
   });
   return cb(newArr);
 }
+
+removeDuplicates(newItems, function (arr) {
+  console.log(arr);
+});
